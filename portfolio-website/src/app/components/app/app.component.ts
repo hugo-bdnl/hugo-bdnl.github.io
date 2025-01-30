c3392a60f3287a39a20e8b6278200b915b90acc6
Hugo Berdinel <115654130+HugolaLicorne@users.noreply.github.com>
2025-01-30 11:02:05 +0100

    update: project component

--- portfolio-website/src/app/components/app/app.component.ts
+++ portfolio-website/src/app/components/app/app.component.ts
@@ -1,11 +1,12 @@
 import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { CompetencesComponent } from '../competences/competences.component';
 import { FormationComponent } from '../formation/formation.component';
 import { MenuComponent } from '../menu/menu.component';
 import { ProjectsComponent } from '../projects/projects.component';
+
 
 @Component({
   standalone: true,
@@ -23,6 +24,7 @@ import { ProjectsComponent } from '../projects/projects.component';
 export class AppComponent implements AfterViewInit {
   constructor(
     private router: Router,
+    private renderer: Renderer2,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
@@ -36,6 +38,20 @@ export class AppComponent implements AfterViewInit {
             element.scrollIntoView({ behavior: 'smooth' });
           }
         }
+      });
+
+      // Ajoute les événements de scroll sur les boutons de compétence
+      const competenceButtons = document.querySelectorAll('.competence-btn');
+      competenceButtons.forEach(button => {
+        this.renderer.listen(button, 'click', () => {
+          const competence = button.getAttribute('data-competence');
+          const targetElement = Array.from(document.querySelectorAll('.competence'))
+            .find(el => el.querySelector('h1')?.textContent === competence);
+
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+          }
+        });
       });
     }
   }
